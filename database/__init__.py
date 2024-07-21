16570c5090d5825cfd2e363700ddd9eefd46d9e1
m.izzuka <450225GA>
2024-07-21 19:50:06 +0300

    database initialization fix

--- database/__init__.py
+++ database/__init__.py
@@ -1,18 +1,28 @@
+import asyncio
+import socket
+from contextlib import closing
+
+from logger import logger
 from sqlalchemy import text
+from sqlalchemy.exc import OperationalError
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
 
 class AsyncDatabaseSession:
+    DB_USER = os.getenv("DB_USER", "postgres")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
+    DB_HOST = os.getenv("DB_HOST", "localhost")
+    DB_NAME = os.getenv("DB_NAME", "dev_db")
+    DB_PORT = os.getenv("DB_PORT", 5432)
+
     def __init__(self):
         self.session_factory = None
         self._engine = None
-        self._db_config = DATABASE_URL
+        self._db_config = self.get_db_connection_string()
         self.init()
 
     async def __aenter__(self):
@@ -30,18 +40,39 @@ class AsyncDatabaseSession:
             self._engine, expire_on_commit=False, class_=AsyncSession
         )
 
+    def get_db_connection_string(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
     async def session_dependency(self) -> AsyncSession:
         async with self.session_factory() as session:
             yield session
 
+    @staticmethod
+    async def check_database_available(retries=5, delay=5):
+        for attempt in range(retries):
+            with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+                if sock.connect_ex((os.getenv('DATABASE_HOST'), int(os.getenv('DATABASE_PORT')))) == 0:
+                    return True
+                else:
+                    logger.warning(f"Попытка {attempt + 1} не удалась")
+                    await asyncio.sleep(delay)
+        return False
+
     async def init_tables(self):
-        async with self._engine.begin() as conn:
-            result = await conn.execute(text("SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname = 'public';"))
-            tables = result.fetchall()
-            if not tables:
-                from .models import Base
-                await conn.run_sync(Base.metadata.create_all)
+        if await self.check_database_available():
+            async with self._engine.begin() as conn:
+                result = await conn.execute(
+                    text("SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname = 'public';"))
+                tables = result.fetchall()
+
+                if not tables:
+                    from .models import Base
+                    await conn.run_sync(Base.metadata.create_all)
+                    logger.info("Таблицы созданы")
+                else:
+                    logger.info("Таблицы уже существуют")
+        else:
+            print("Не удалось подключиться к базе данных")
 
 
 db = AsyncDatabaseSession()
-
